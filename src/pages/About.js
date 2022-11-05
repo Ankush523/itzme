e079ccb813d38cf04dfbd234b77805417c76eb54
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-11-05 17:45:10 +0530

    tech skills included

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div className='px-[100px] my-[100px] snap-start snap-always'>
-        <div className='flex flex-col text-left my-[90px]'>
+        <div className='flex flex-col text-left my-[50px]'>
         <label className='text-5xl'>I love to create</label>
         <label className='text-5xl'>something</label>
         <label className='text-5xl'>simple and useful</label>
@@ -12,7 +12,10 @@ const About = () => {
             <br/>
             <label className='text-6xl font-semibold mb-[20px]'>About Me</label>
             <br/>
-            <label className='text-left text-2xl px-[300px]'>I am a lifelong learner with experience in Full Stack Development in Web2 and Web3. I am extremly passionate about writing code that has an impact and can bring ideas to life.
+            <label className='text-left text-2xl px-[300px]'>Presently a sophomore at SRMIST, Chennai, am persuing Computer Science Engineering with specialization in AI ML at the same. I am a lifelong learner with experience in Full Stack Development in Web2 and Web3. I am extremly passionate about writing code that has an impact and can bring ideas to life.
+            <br/>
+            <br/>
+            My tech skills includes C, C++, Python, HTML, Tailwind CSS, ReactJS, NodeJS, ExpressJS, MySQL, MongoDB, Solidity and Ethersjs.
             <br/>
             <br/>
             In addition to development, I enjoy playing LoL, travelling, listening to music, reading fiction and helping communities and individuals.</label>
